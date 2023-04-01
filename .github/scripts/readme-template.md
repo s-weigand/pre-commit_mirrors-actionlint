28f1a6e4835a636208ee62b0f40e3bb89a9b0e0c
s-weigand <s.weigand.phy@gmail.com>
2023-04-01 17:10:39 +0200

    ✨ Added update script and templates

---
+++ .github/scripts/readme-template.md
@@ -0,0 +1,18 @@
+# actionlint mirror
+
+Mirror of `actionlint` for pre-commit.
+
+For pre-commit: see https://github.com/pre-commit/pre-commit
+
+For actionlint: see https://github.com/rhysd/actionlint
+
+### Using actionlint with pre-commit
+
+Add this to your `.pre-commit-config.yaml`:
+
+```yaml
+- repo: https://github.com/s-weigand/pre-commit_mirrors-actionlint
+  rev: "{{tag}}"
+  hooks:
+    - id: actionlint
+```
---
+++ .github/scripts/update-hook.py
@@ -0,0 +1,56 @@
+"""Render templates to update to new tag."""
+import os
+from pathlib import Path
+
+import httpx
+from jinja2 import Template
+
+HERE = Path(__file__).parent
+REPO_ROOT = HERE.parent.parent
+
+
+def get_latest_tag() -> str:
+    """Get latest tag of https://github.com/rhysd/actionlint .
+
+    Returns
+    -------
+    str
+        Latest tag string.
+    """
+    resp = httpx.get("https://api.github.com/repos/rhysd/actionlint/tags")
+    return resp.json()[0]["name"]
+
+
+def render_templates(tag: str) -> None:
+    """Renders templates with new ``tag`` if the value has changed.
+
+    Parameters
+    ----------
+    tag: str
+        _description_
+    """
+    version_file_path = REPO_ROOT / ".version"
+    current_version = version_file_path.read_text(encoding="utf8")
+    if tag != current_version:
+        hook_template = Template(
+            (HERE / ".pre-commit-hooks-template.yaml").read_text(encoding="utf8")
+        )
+        (REPO_ROOT / ".pre-commit-hooks.yaml").write_text(
+            hook_template.render({"tag": tag}), encoding="utf8"
+        )
+        readme_template = Template(
+            (HERE / "readme-template.md").read_text(encoding="utf8")
+        )
+        (REPO_ROOT / "README.md").write_text(
+            readme_template.render({"tag": tag}), encoding="utf8"
+        )
+        version_file_path.write_text(tag, encoding="utf8")
+        gh_output = os.getenv("GITHUB_OUTPUT", None)
+        if gh_output is not None:
+            with Path(gh_output).open("a", encoding="utf8") as f:
+                f.writelines([f"tag={tag}", f"current_version={current_version}"])
+
+
+if __name__ == "__main__":
+    tag = get_latest_tag()
+    render_templates(tag)
